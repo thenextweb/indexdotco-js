639f841869899405e6cd89530b705407fb052f47
Otto <otto@thenextweb.com>
2016-09-14 17:24:41 +0200

    Use the CDN to serve company data

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ var config = {}
 config.prefix = 'idc-';
 config.cardPrefix = config.prefix+'card-';
 config.version = '0.0.2';
-config.endpoint = 'https://index.co/api/';
+config.endpoint = 'https://index.tnwcdn.com/api/';
 
 export default config;
